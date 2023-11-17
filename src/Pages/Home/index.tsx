ffd338e356f737035525b59bc0039304f4eacfbd
Manuel Segarra <manuel@segarra.pt>
2023-11-17 01:44:06 +0000

    added animation for my name

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -3,7 +3,7 @@ import "./index.css"
 
 function HomePage() {
   return (
-    <div id="intro">
+    <div id="intro" className="text-container">
       <p id="name">MANUEL SEGARRA</p>
       <p id="title">SOFTWARE DEVELOPER, FULL-STACK WEB DEVELOPER</p>
     </div>
