576a5d12dc7e78417501051e56281fb790dc8d88
Manuel Segarra <manuel@segarra.pt>
2023-11-18 13:59:47 +0000

    implemented page transition animations using framer-motion

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import "./App.css";
-import { Routes, Route } from "react-router-dom";
-
-// Import all the pages
-import Navbar from "./components/Navbar"
+// App.tsx
+import React from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
+import { AnimatePresence } from "framer-motion";
+import Navbar from "./components/Navbar";
 import AboutPage from "./pages/About";
 import CertificationsPage from "./pages/Certifications";
 import ContactPage from "./pages/Contact";
@@ -10,22 +10,27 @@ import HomePage from "./pages/Home";
 import ProjectsPage from "./pages/Projects";
 import ResumePage from "./pages/Resume";
 import SkillsPage from "./pages/Skills";
+import "./App.css";
 
-function App() {
+const App: React.FC = () => {
+  const location = useLocation();
+
   return (
     <div>
       <Navbar />
-      <Routes>
-        <Route path="/about" element={<AboutPage />} />
-        <Route path="/certifications" element={<CertificationsPage />} />
-        <Route path="/contact" element={<ContactPage />} />
-        <Route path="/" element={<HomePage />} />
-        <Route path="/projects" element={<ProjectsPage />} />
-        <Route path="/resume" element={<ResumePage />} />
-        <Route path="/skills" element={<SkillsPage />} />
-      </Routes>
+      <AnimatePresence mode="wait">
+        <Routes location={location} key={location.pathname}>
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/certifications" element={<CertificationsPage />} />
+          <Route path="/contact" element={<ContactPage />} />
+          <Route path="/" element={<HomePage />} />
+          <Route path="/projects" element={<ProjectsPage />} />
+          <Route path="/resume" element={<ResumePage />} />
+          <Route path="/skills" element={<SkillsPage />} />
+        </Routes>
+      </AnimatePresence>
     </div>
   );
-}
+};
 
 export default App;
--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,12 +1,20 @@
 import React from "react";
-import "./index.css"
+import "./index.css";
+import { motion as m } from "framer-motion";
 
 function HomePage() {
   return (
-    <div id="intro" className="text-container">
+    <m.div
+      id="intro"
+      className="text-container"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 1.5, ease: "easeOut" }}
+      exit={{ opacity: 0 }}
+    >
       <p id="name">MANUEL SEGARRA</p>
       <p id="title">SOFTWARE DEVELOPER, FULL-STACK WEB DEVELOPER</p>
-    </div>
+    </m.div>
   );
 }
 
